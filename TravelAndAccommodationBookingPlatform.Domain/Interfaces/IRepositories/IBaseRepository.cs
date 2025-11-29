81938ad1c71a34f4cd23adc3eebe4d60a478b08f
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-11-29 12:18:43 +0200

    add BaseRepository

---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRepositories/IBaseRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
+    public interface IBaseRepository<T> where T : class
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<List<T>> GetAllAsync();
+        Task AddAsync(T entity);
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(int id);
+    }
