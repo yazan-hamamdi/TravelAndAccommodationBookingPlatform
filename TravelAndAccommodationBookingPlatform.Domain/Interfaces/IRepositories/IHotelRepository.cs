245bd032cf7bedf5d7c64199d591ed9a5640701c
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-01 11:36:12 +0200

    add Search feature to hotel repository

---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRepositories/IHotelRepository.cs
@@ -0,0 +1,8 @@
+﻿using TravelAndAccommodationBookingPlatform.Domain.Entities;
+using TravelAndAccommodationBookingPlatform.Domain.Models.SearchDtos;
+
+namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
+public interface IHotelRepository : IBaseRepository<Hotel>
+{
+    Task<(IEnumerable<Hotel>, int TotalCount)> SearchHotelsAsync(SearchRequestDto searchRequest, int pageSize, int pageNumber);
+}
