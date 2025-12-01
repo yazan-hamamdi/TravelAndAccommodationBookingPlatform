f30e729957f121c20d47c380663aec837395e752
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-01 13:17:57 +0200

    add Booking repository

---
+++ TravelAndAccommodationBookingPlatform.Db/Repositories/BookingRepository.cs
@@ -0,0 +1,14 @@
+﻿using TravelAndAccommodationBookingPlatform.Db.DbContext;
+using TravelAndAccommodationBookingPlatform.Domain.Entities;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
+
+namespace TravelAndAccommodationBookingPlatform.Db.Repositories;
+public class BookingRepository : BaseRepository<Booking>, IBookingRepository
+{
+    private readonly TravelAndAccommodationBookingDbContext _context;
+
+    public BookingRepository(TravelAndAccommodationBookingDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
