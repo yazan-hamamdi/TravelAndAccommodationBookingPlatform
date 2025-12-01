82abecece36bf343120bfda2f936e54b3cfefc05
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-01 11:21:37 +0200

    add Pagination Service

---
+++ TravelAndAccommodationBookingPlatform.Db/DbServices/PaginationService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IServices;
+
+namespace TravelAndAccommodationBookingPlatform.Db.DbServices;
+public class PaginationService : IPaginationService
+{
+    public async Task<(IEnumerable<T> Items, int TotalCount)> PaginateAsync<T>(
+        IQueryable<T> query, int pageSize, int pageNumber) where T : class
+    {
+        if (pageNumber <= 0 || pageSize <= 0)
+        {
+            var totalCount0 = await query.CountAsync();
+            var items0 = await query
+                .ToListAsync();
+            return (items0, totalCount0);
+        }
+
+
+        var totalCount = await query.CountAsync();
+        var items = await query
+            .Skip((pageNumber - 1) * pageSize)
+            .Take(pageSize)
+            .AsNoTracking()
+            .ToListAsync();
+
+        return (items, totalCount);
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/IServices/IPaginationService.cs
@@ -0,0 +1,7 @@
+﻿using System;
+using System.Collections.Generic;
+namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.IServices;
+public interface IPaginationService
+{
+    public Task<(IEnumerable<T> Items, int TotalCount)> PaginateAsync<T>(IQueryable<T> query, int pageSize, int pageNumber) where T : class;
+}
