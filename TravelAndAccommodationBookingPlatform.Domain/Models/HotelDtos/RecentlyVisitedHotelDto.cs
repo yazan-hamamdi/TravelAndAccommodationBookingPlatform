59c62849a8c038b8d4bc8e332068f3e6f30cab06
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-02 11:14:14 +0200

    Add CheckoutDto

--- TravelAndAccommodationBookingPlatform.Domain/Models/HotelDtos/RecentlyVisitedHotelDto.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Models/HotelDtos/RecentlyVisitedHotelDto.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TravelAndAccommodationBookingPlatform.Domain.Models.HotelDtos
+﻿namespace TravelAndAccommodationBookingPlatform.Domain.Models.HotelDtos;
+public class RecentlyVisitedHotelDto
 {
-    internal class Class1
-    {
-    }
+    public Guid HotelId { get; set; }
+    public string HotelName { get; set; }
+    public string City { get; set; }
+    public string ThumbnailUrl { get; set; }
+    public int StarRating { get; set; }
+    public decimal PricePerNight { get; set; }
 }
