3ab118b388012c329e0732b1b71206a1f42b392e
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-02 10:19:33 +0200

    Add HotelController with summaries and validators

---
+++ TravelAndAccommodationBookingPlatform.API/Validators/HotelValidators/CreateHotelValidator.cs
@@ -0,0 +1,33 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.Domain.Models.HotelDtos;
+
+namespace TravelAndAccommodationBookingPlatform.API.Validators.HotelValidators;
+public class CreateHotelValidator : GenericValidator<CreateHotelDto>
+{
+    public CreateHotelValidator()
+    {
+        RuleFor(x => x.CityId)
+            .NotEmpty().WithMessage("CityId is required");
+        RuleFor(x => x.HotelName)
+            .NotEmpty().WithMessage("HotelName is required")
+            .MaximumLength(100).WithMessage("HotelName cannot be more than 100 characters");
+        RuleFor(x => x.Description)
+            .MaximumLength(500).WithMessage("Description cannot be more than 500 characters");
+        RuleFor(x => x.StarRating)
+            .NotEmpty().WithMessage("StarRating is required")
+            .InclusiveBetween(1, 5).WithMessage("StarRating must be between 1 and 5");
+        RuleFor(x => x.OwnerId)
+            .NotEmpty().WithMessage("OwnerId is required");
+        RuleFor(x => x.PhoneNumber)
+            .NotEmpty().WithMessage("PhoneNumber is required");
+        RuleFor(x => x.Address)
+            .NotEmpty().WithMessage("Address is required")
+            .MaximumLength(200).WithMessage("Address cannot be more than 200 characters");
+        RuleFor(x => x.Latitude)
+            .NotEmpty().WithMessage("Latitude is required");
+        RuleFor(x => x.Longitude)
+            .NotEmpty().WithMessage("Longitude is required");
+        RuleFor(x => x.ThumbnailUrl)
+            .MaximumLength(200).WithMessage("ThumbnailUrl cannot be more than 200 characters");
+    }
+}
