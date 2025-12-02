2a9eaf6637fe9c087fb25e8f282b297a43ebcfd3
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-02 11:50:36 +0200

    Add BookingController and Checkout request validators

---
+++ TravelAndAccommodationBookingPlatform.API/Controllers/BookingsController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TravelAndAccommodationBookingPlatform.API.Validators.AuthValidators;
+using TravelAndAccommodationBookingPlatform.API.Validators.BookingValidators;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IServices;
+using TravelAndAccommodationBookingPlatform.Domain.Models.BookingDtos;
+
+namespace TravelAndAccommodationBookingPlatform.API.Controllers;
+[ApiController]
+[Route("api/bookings")]
+[Authorize(Policy = "UserOrAdmin")]
+public class BookingsController : Controller
+{
+    private readonly IBookingService _bookingService;
+
+    public BookingsController(IBookingService bookingService)
+    {
+        _bookingService = bookingService;
+    }
+
+    /// <summary>
+    /// move the cart items to the booking and create payment
+    /// </summary>
+    /// <param name="requestDto"> the checkout request </param>
+    /// <returns> the approval url and the booking id </returns>
+    /// <response code="200">returns the approval url and the booking id</response>
+    /// <response code="401">if the user is not authenticated</response>
+    /// <response code="403">if the user id in the token does not match the user id in the request</response>
+    /// <response code="404">if the user id is not valid or the user does not have a cart</response>
+    /// <response code="409">if the room is not available for the selected dates</response>
+    [ValidateUserId]
+    [HttpPost("checkout")]
+    public async Task<CheckoutDto> CheckoutAsync([FromBody] CheckoutRequestDto requestDto)
+    {
+        var validator = new CheckoutValidator();
+        await validator.ValidateAndThrowCustomExceptionAsync(requestDto);
+        return await _bookingService.CreateBookingFromCartAsync(requestDto);
+    }
+}
---
+++ TravelAndAccommodationBookingPlatform.API/Validators/BookingValidators/CheckoutValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.Domain.Models.BookingDtos;
+
+namespace TravelAndAccommodationBookingPlatform.API.Validators.BookingValidators;
+public class CheckoutValidator : GenericValidator<CheckoutRequestDto>
+{
+    public CheckoutValidator()
+    {
+        RuleFor(x => x.UserId).NotEmpty().WithMessage("UserId is required");
+    }
+}
