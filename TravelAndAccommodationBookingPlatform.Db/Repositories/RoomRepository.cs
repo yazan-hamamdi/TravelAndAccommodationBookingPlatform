faa10f57c60592d9525a2c9aac122d0f3a7b3ee0
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-03 23:48:58 +0200

    Add GetAllPagedAsync and GetRoomByHotelAndNumberAsync methods to RoomRepository

--- TravelAndAccommodationBookingPlatform.Db/Repositories/RoomRepository.cs
+++ TravelAndAccommodationBookingPlatform.Db/Repositories/RoomRepository.cs
@@ -3,15 +3,19 @@ using TravelAndAccommodationBookingPlatform.Db.DbContext;
 using TravelAndAccommodationBookingPlatform.Domain.Entities;
 using TravelAndAccommodationBookingPlatform.Domain.Enums;
 using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
+using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IServices;
 
 namespace TravelAndAccommodationBookingPlatform.Db.Repositories;
 public class RoomRepository : BaseRepository<Room>, IRoomRepository
 {
     private readonly TravelAndAccommodationBookingDbContext _context;
+    private readonly IPaginationService _paginationService;
 
-    public RoomRepository(TravelAndAccommodationBookingDbContext context) : base(context)
+
+    public RoomRepository(TravelAndAccommodationBookingDbContext context, IPaginationService paginationService) : base(context)
     {
         _context = context;
+        _paginationService = paginationService;
     }
 
     public async Task<Room?> GetRoomIfAvailableAsync(Guid roomId, DateTime checkInDate, DateTime checkOutDate)
@@ -27,5 +31,18 @@ public class RoomRepository : BaseRepository<Room>, IRoomRepository
                                           b.CheckOutDate <= checkInDate ||
                                           b.CheckInDate >= checkOutDate) &&
                                       r.Availability == true);
+    }
+
+    public async Task<(IEnumerable<Room> Items, int TotalCount)> GetAllPagedAsync(int pageNumber, int pageSize)
+    {
+        var rooms = _context.Rooms.AsQueryable();
+        var (paginatedRooms, totalCount) = await _paginationService.PaginateAsync(rooms, pageSize, pageNumber);
+        return (paginatedRooms, totalCount);
+    }
+
+    public async Task<Room?> GetRoomByHotelAndNumberAsync(Guid hotelId, string roomNumber)
+    {
+        return await _context.Rooms
+            .FirstOrDefaultAsync(r => r.HotelId == hotelId && r.RoomNumber == roomNumber);
     }
 }
