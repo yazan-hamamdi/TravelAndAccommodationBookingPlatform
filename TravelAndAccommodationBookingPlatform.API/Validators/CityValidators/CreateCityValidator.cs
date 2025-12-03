d5e7658667375193bffaf4295608966308ddc9c9
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-03 21:57:13 +0200

    Add city controller

---
+++ TravelAndAccommodationBookingPlatform.API/Validators/CityValidators/CreateCityValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using TravelAndAccommodationBookingPlatform.Domain.Models.CityDtos;
+
+namespace TravelAndAccommodationBookingPlatform.API.Validators.CityValidators;
+public class CreateCityValidator : GenericValidator<CreateCityDto>
+{
+    public CreateCityValidator()
+    {
+        RuleFor(x => x.CityName)
+            .NotEmpty()
+            .WithMessage("City name is required.")
+            .MaximumLength(50)
+            .WithMessage("City name must not exceed 50 characters.");
+
+        RuleFor(x => x.Country)
+            .NotEmpty()
+            .WithMessage("Country is required.")
+            .MaximumLength(50)
+            .WithMessage("Country must not exceed 50 characters.");
+
+        RuleFor(x => x.PostCode)
+            .MaximumLength(10)
+            .WithMessage("Post code must not exceed 10 characters.");
+
+        RuleFor(x => x.ThumbnailUrl)
+            .MaximumLength(200)
+            .WithMessage("Thumbnail URL must not exceed 200 characters.");
+    }
+}
