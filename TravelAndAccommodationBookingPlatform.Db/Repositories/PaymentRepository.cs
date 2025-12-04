345188dead6a65e8954620881910489b25405adf
yazan-hamamdi <yazan.hamamdi9@gmail.com>
2025-12-04 13:13:54 +0200

    Add repository methods for retrieving and updating payments with booking details

--- TravelAndAccommodationBookingPlatform.Db/Repositories/PaymentRepository.cs
+++ TravelAndAccommodationBookingPlatform.Db/Repositories/PaymentRepository.cs
@@ -1,14 +1,40 @@
-﻿using TravelAndAccommodationBookingPlatform.Db.DbContext;
+﻿using Microsoft.EntityFrameworkCore;
+using TravelAndAccommodationBookingPlatform.Db.DbContext;
 using TravelAndAccommodationBookingPlatform.Domain.Entities;
 using TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
 
 namespace TravelAndAccommodationBookingPlatform.Db.Repositories;
-public class PaymentRepository : BaseRepository<Payment>, IPaymentRepository
+public class PaymentRepository : IPaymentRepository
 {
     private readonly TravelAndAccommodationBookingDbContext _context;
 
-    public PaymentRepository(TravelAndAccommodationBookingDbContext context) : base(context)
+    public PaymentRepository(TravelAndAccommodationBookingDbContext context)
     {
         _context = context;
+    }
+
+    public async Task<Payment?> GetPaymentWithBookingDetailsByIdAsync(Guid paymentId)
+    {
+        return await _context.Payments
+            .Include(p => p.Booking)
+            .ThenInclude(b => b.User)
+            .Include(p => p.Booking)
+            .ThenInclude(b => b.BookingDetails)
+            .ThenInclude(bd => bd.Room)
+            .FirstOrDefaultAsync(p => p.PaymentId == paymentId);
+    }
+
+    public async Task<Payment?> GetPaymentWithBookingByIdAsync(Guid paymentId)
+    {
+        return await _context.Payments
+            .Include(p => p.Booking)
+            .ThenInclude(b => b.User)
+            .FirstOrDefaultAsync(p => p.PaymentId == paymentId);
+    }
+
+    public async Task UpdatePaymentAsync(Payment payment)
+    {
+        _context.Payments.Update(payment);
+        await _context.SaveChangesAsync();
     }
 }
--- TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRepositories/IPaymentRepository.cs
+++ TravelAndAccommodationBookingPlatform.Domain/Interfaces/IRepositories/IPaymentRepository.cs
@@ -1,6 +1,9 @@
 ﻿using TravelAndAccommodationBookingPlatform.Domain.Entities;
 
 namespace TravelAndAccommodationBookingPlatform.Domain.Interfaces.IRepositories;
-public interface IPaymentRepository : IBaseRepository<Payment>
+public interface IPaymentRepository
 {
+    Task<Payment?> GetPaymentWithBookingDetailsByIdAsync(Guid paymentId);
+    Task<Payment?> GetPaymentWithBookingByIdAsync(Guid paymentId);
+    Task UpdatePaymentAsync(Payment payment);
 }
